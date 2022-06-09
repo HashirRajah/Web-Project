1bd25ff74cf95272ad49f3b4a21b5870840e5d07
Hashir Rajah <mhashirhrajah00@gmail.com>
2022-06-09 22:56:05 +0400

    Added jquery script tag in docEnd.php for both front-end and back-end

---
+++ back-end/Admin/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+    //page meta data
+    $title = "Dashboard";
+    $moreStyle = false;
+
+?>
+<?php include("./include/docStart.php"); ?>
+<?php include("./include/navbar.php"); ?>
+
+<?php include("./include/docEnd.php"); ?>
