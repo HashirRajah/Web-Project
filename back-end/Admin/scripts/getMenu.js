084a0f45013f7e5bb8dffba297108a6555277486
Divesh <divesh.nugessur04@gmail.com>
2022-07-22 21:13:16 +0400

    menu

--- back-end/Admin/scripts/getMenu.js
+++ back-end/Admin/scripts/getMenu.js
@@ -1,7 +1,12 @@
 //after dom is loaded
 $(document).ready(function(){
+
+    getStarters();
+    getMainCourse();
+    getDessert();
+    getDrinks();
    
-    $(".options").click(function(){
+    $(document).on("click", ".options", function(){
 
         $("ul.list-group").css({"display":"none"});
 
@@ -15,11 +20,205 @@ $(document).ready(function(){
         
     });
 
-    $(".edit").click(function(){
+    $(document).on("click", ".edit", function(){
 
         window.location.href = `editMenu.php?item_id=`;
 
     });
+
 });
 
+
+//functions
+function getStarters() {
+    //api url
+    let URL = "http://localhost/Web-Project/back-end/Admin/api/menu?category=Starter";
+    $.ajax({
+        url: URL, 
+        accepts: "application/json",
+        method: "GET",
+        cache: false, 
+        error: function(xhr){
+            alert("An error occured: " + xhr.status + " " + xhr.statusText);
+        }
+    }).done(function(data){
+        data = JSON.parse(data);
+        if(data['menu-items'] !== "no-data"){
+            let html = ``;
+           
+            //
+            $.each(data['menu-items'], function(i, item){
+                html += `
+                   <div class="food-items">
+                        <img src= "../../front-end.$starters["link"]; " >
+                        <div class="details">
+                            <div class="details-sub">
+                                <h5>${item.name}</h5>
+                                <h1 class="price">RS${item.unit_price}</h1>
+                            </div>
+                            <p class="lead">${item.description}</p>
+                            <i class="options bi bi-three-dots-vertical"></i>
+
+                            <ul style="display:none;" class="list-group">
+                                <li class="list-group-item edit">Edit</li>
+                                <li class="list-group-item">Delete</li> 
+                            </ul>   
+                        </div>
+                    </div>
+                `;
+            });
+
+            //add content to table
+            $("#starters").html(html);
+            //
+            
+        } else {
+            
+        }
+    });
+};
+
+function getMainCourse() {
+    //api url
+    let URL = "http://localhost/Web-Project/back-end/Admin/api/menu?category=Main Course";
+    $.ajax({
+        url: URL, 
+        accepts: "application/json",
+        method: "GET",
+        cache: false, 
+        error: function(xhr){
+            alert("An error occured: " + xhr.status + " " + xhr.statusText);
+        }
+    }).done(function(data){
+        data = JSON.parse(data);
+        if(data['menu-items'] !== "no-data"){
+            let html = ``;
+           
+            //
+            $.each(data['menu-items'], function(i, item){
+                html += `
+                   <div class="food-items">
+                        <img src= "../../front-end.$starters["link"]; " >
+                        <div class="details">
+                            <div class="details-sub">
+                                <h5>${item.name}</h5>
+                                <h1 class="price">RS${item.unit_price}</h1>
+                            </div>
+                            <p class="lead">${item.description}</p>
+                            <i class="options bi bi-three-dots-vertical"></i>
+
+                            <ul style="display:none;" class="list-group">
+                                <li class="list-group-item edit">Edit</li>
+                                <li class="list-group-item">Delete</li> 
+                            </ul>   
+                        </div>
+                    </div>
+                `;
+            });
+
+            //add content to table
+            $("#main-course").html(html);
+            //
+            
+        } else {
+            
+        }
+    });
+};
+
+function getDessert() {
+    //api url
+    let URL = "http://localhost/Web-Project/back-end/Admin/api/menu?category=Dessert";
+    $.ajax({
+        url: URL, 
+        accepts: "application/json",
+        method: "GET",
+        cache: false, 
+        error: function(xhr){
+            alert("An error occured: " + xhr.status + " " + xhr.statusText);
+        }
+    }).done(function(data){
+        data = JSON.parse(data);
+        if(data['menu-items'] !== "no-data"){
+            let html = ``;
+           
+            //
+            $.each(data['menu-items'], function(i, item){
+                html += `
+                   <div class="food-items">
+                        <img src= "../../front-end.$starters["link"]; " >
+                        <div class="details">
+                            <div class="details-sub">
+                                <h5>${item.name}</h5>
+                                <h1 class="price">RS${item.unit_price}</h1>
+                            </div>
+                            <p class="lead">${item.description}</p>
+                            <i class="options bi bi-three-dots-vertical"></i>
+
+                            <ul style="display:none;" class="list-group">
+                                <li class="list-group-item edit">Edit</li>
+                                <li class="list-group-item">Delete</li> 
+                            </ul>   
+                        </div>
+                    </div>
+                `;
+            });
+
+            //add content to table
+            $("#dessert").html(html);
+            //
+            
+        } else {
+            
+        }
+    });
+};
+
+function getDrinks() {
+    //api url
+    let URL = "http://localhost/Web-Project/back-end/Admin/api/menu?category=Drinks";
+    $.ajax({
+        url: URL, 
+        accepts: "application/json",
+        method: "GET",
+        cache: false, 
+        error: function(xhr){
+            alert("An error occured: " + xhr.status + " " + xhr.statusText);
+        }
+    }).done(function(data){
+        data = JSON.parse(data);
+        if(data['menu-items'] !== "no-data"){
+            let html = ``;
+           
+            //
+            $.each(data['menu-items'], function(i, item){
+                html += `
+                   <div class="food-items">
+                        <img src= "../../front-end.$starters["link"]; " >
+                        <div class="details">
+                            <div class="details-sub">
+                                <h5>${item.name}</h5>
+                                <h1 class="price">RS${item.unit_price}</h1>
+                            </div>
+                            <p class="lead">${item.description}</p>
+                            <i class="options bi bi-three-dots-vertical"></i>
+
+                            <ul style="display:none;" class="list-group">
+                                <li class="list-group-item edit">Edit</li>
+                                <li class="list-group-item">Delete</li> 
+                            </ul>   
+                        </div>
+                    </div>
+                `;
+            });
+
+            //add content to table
+            $("#drinks").html(html);
+            //
+            
+        } else {
+            
+        }
+    });
+};
 
