09cedbbf1182b00505e167e9777ed3df02c0e7a4
Divesh <divesh.nugessur04@gmail.com>
2022-07-29 21:20:27 +0400

    f

--- front-end/view_order.php
+++ front-end/view_order.php
@@ -8,6 +8,28 @@
 
     include_once("./include/functions.php");
     include_once("./database/db_connect.php");
+
+       
+     
+        
+        
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $username = $_SESSION["user-logged-in"]["username"];
+       
+        $sql = "SELECT * FROM orders WHERE username = $username AND status = 'pending'" ;
+        $result = $conn->query($sql);
+                //fetch all results
+        $order = $result->fetchAll(PDO::FETCH_ASSOC);
+       
+        $sql = "SELECT * FROM food_order_details WHERE order_id = {$order['order_id']}";
+        $result = $conn->query($sql);
+                //fetch all results
+        $items = $result->fetchAll(PDO::FETCH_ASSOC);
+        
+
+        
+
+
 ?>
 <?php include_once("./include/docStart.php"); ?>
 <?php include_once("./include/navbar.php"); ?>
@@ -17,7 +39,36 @@
     <h2> Hello <span> <?php echo $_SESSION["user-logged-in"]["username"]; ?> </span></h2>
     <h1 class="lead">No current orders</h1>
 </div>
+
 <section >
+<table border=1>
+        <caption>Orders</caption>
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Date</th>
+            <th>Time </th>
+            <th>Discount</th>
+            <th>type</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+            while($reservations->fetch(PDO::FETCH_ASSOC))
+            { 
+            
+            echo '<td>' . $order['id'] . '</td>';
+            echo '<td>' . $order['date'] . '</td>';
+            echo '<td>' . $order['time'] . '</td>';
+            echo '<td>' . $order['discount'] . '</td>';
+            echo '<td>' . $order['status'] . '</td>';
+            echo '<td>' . $order['type'] . '</td>';
+            }//end while
+        ?>  
+        </tbody>
+    </table>
+
 </section>
+
 <?php include_once("./include/footer.php"); ?>
 <?php include_once("./include/docEnd.php"); ?>
--- front-end/view_reservation.php
+++ front-end/view_reservation.php
@@ -9,6 +9,16 @@
 
     include_once("./include/functions.php");
     include_once("./database/db_connect.php");
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $username = $_SESSION["user-logged-in"]["username"];
+   
+    $sql = "SELECT * FROM reservations WHERE username = $username AND status = 'pending'" ;
+    $result = $conn->query($sql);
+            //fetch all results
+    $reservations = $result->fetchAll(PDO::FETCH_ASSOC);
+   
+ 
 ?>
 
 
@@ -22,6 +32,33 @@
     <h1 class="lead">No current reservations</h1>
 </div>
 <section >
+    <table border=1>
+        <caption>Reservations</caption>
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Date</th>
+            <th>Time Slot</th>
+            <th>No of People</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+            while($reservations->fetch(PDO::FETCH_ASSOC))
+            { 
+            
+            echo '<td>' . $reservations['id'] . '</td>';
+            echo '<td>' . $reservations['date'] . '</td>';
+            echo '<td>' . $reservations['time_slot'] . '</td>';
+            echo '<td>' . $reservations['no_of_people'] . '</td>';
+            echo '<td>' . $reservations['status'] . '</td>';
+
+            
+            }//end while
+        ?>  
+        </tbody>
+    </table>
 </section>
 <?php include_once("./include/footer.php"); ?>
 <?php include_once("./include/docEnd.php"); ?>
